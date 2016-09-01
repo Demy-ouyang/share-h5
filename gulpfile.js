a2dc421a06d87158f2ff5ae23ad2e58e5906c486
Demy <18911887579@sina.cn>
2016-09-01 15:13:29 +0800

    add gulp task-less
    ###css编译成功

--- gulpfile.js
+++ gulpfile.js
@@ -4,3 +4,14 @@
 //导入工具包
 var gulp = require('gulp'),
     less = require('gulp-less');
+//定义less编译任务
+gulp.task('less',function(){
+    gulp.src('src/less/style.less')//该任务针对的文件
+        .pipe(less())//该任务调用
+        .pipe(gulp.dest('src/css'))//将会在src/css下生成style.css
+});
+gulp.task('default',['less', 'elseTask']); //定义默认任务 elseTask为其他任务，该示例没有定义elseTask任务
+
+//gulp.task(name[, deps], fn) 定义任务  name：任务名称 deps：依赖任务名称 fn：回调函数
+//gulp.src(globs[, options]) 执行任务处理的文件  globs：处理的文件路径(字符串或者字符串数组)
+//gulp.dest(path[, options]) 处理完后文件生成路径
